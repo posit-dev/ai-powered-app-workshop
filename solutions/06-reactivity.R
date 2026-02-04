827394e2d56df1b7527575c8294da707e4d27f27
Garrett Grolemund <garrett@posit.co>
2026-02-04 10:37:08 -0600

    Adds solutions

---
+++ solutions/06-reactivity.R
@@ -0,0 +1,27 @@
+# 6. Change a value to an input value to create reactivity
+
+library(shiny)
+library(bslib)
+
+ui <- page_sidebar(
+  sidebar = sidebar(
+    sliderInput( 
+      "n", 
+      "Number of observations", 
+      min = 1, 
+      max = 100, 
+      value = 20
+    )
+  ), 
+    plotOutput("hist")
+)
+
+server <- function(input, output) {
+
+  output$hist <- renderPlot({
+    hist(rnorm(input$n), xlim = c(-4, 4))
+  })
+
+}
+
+shinyApp(ui, server)
---
+++ solutions/10-querychat.R
@@ -0,0 +1,39 @@
+# 10. Make it querychat
+
+library(shiny)
+library(bslib)
+library(DT)
+library(querychat)
+
+source("mortality_map.R")
+georgia_mortality <- read.csv("data/georgia_mortality.csv")
+georgia_population <- read.csv("data/georgia_population.csv")
+
+# Step 1: Initialize QueryChat
+qc <- QueryChat$new(georgia_mortality, client = "anthropic/claude-sonnet-4-5-20250929", greeting = "Hi")
+
+
+ui <- page_sidebar(
+  sidebar = qc$sidebar(), # Step 2: Add a chat to the sidebar
+    leafletOutput("map", height = "500px"),
+    DTOutput("table")
+)
+# Step 3: Use reactive values in server
+server <- function(input, output) {
+  qc_vals <- qc$server()
+
+  dataset <- reactive(
+    qc_vals$df()
+  )
+
+  output$map <- renderLeaflet({
+    create_mortality_map(dataset(), georgia_population)
+  })
+
+  output$table <- DT::renderDT({
+    datatable(dataset(), options = list(pageLength = 20, scrollY = "800px"))
+  })
+
+}
+
+shinyApp(ui, server)
